c7999ca7eb6ee00a6d6653c4d4e3077163ca7d85
Oscar Arroyo Vega <arroscar@gmail.com>
2025-07-30 16:39:35 +0200

    refactor: improve ragsytem structure

--- services/RAG-chat/src/main.py
+++ services/RAG-chat/src/main.py
@@ -2,22 +2,29 @@ from loguru import logger
 from fastapi import FastAPI, status
 from src.config import get_settings
 
-logger.info(f"Using DB: {settings.postgres_url}")
 
+# get the settings
 settings = get_settings()
 
+# create the app
 app = FastAPI(
     title="RAG-chat",
     description="RAG-chat is a Chatbot that uses RAG to answer questions about musical life events",
-    version="0.1.0"
+    version="0.1.0",
 )
+
 
 @app.get("/health", status_code=status.HTTP_200_OK)
 def health():
-    return {"msg": "RAG-chat is running, healthy and ready to answer questions", "user": settings.postgres_user}
+    return {
+        "msg": "RAG-chat is running, healthy and ready to answer questions",
+        "user": settings.postgres_user,
+    }
+
 
 if __name__ == "__main__":
     import uvicorn
+
     logger.info("Starting RAG-chat server...")
-    uvicorn.run(app, host="0.0.0.0", port=8000) # nosec
+    uvicorn.run(app, host="0.0.0.0", port=8000)  # nosec
     logger.info("RAG-chat server started")
